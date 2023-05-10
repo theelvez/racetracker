5abb667456042e0eb89ba0030b807c772d7af860
theelvez <80179310+theelvez@users.noreply.github.com>
2023-05-10 05:49:45 -0700

    Add files via upload

--- Arduino/speedtracker/v2/SpeedTracker_esp32s_uart.ino
+++ Arduino/speedtracker/v2/SpeedTracker_esp32s_uart.ino
@@ -1,15 +1,18 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
 #include <SPI.h>
 #include <SD.h>
-#include <HTTPClient.h>
-#include <WiFi.h>
 #include <Wire.h>
 #include "gps_helpers.h"
 #include <Adafruit_PN532.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_ST7789.h>
-#include <Fonts/FreeMonoBold9pt7b.h>
+#include <U8g2lib.h>
 
-#define ST_USE_ST7789_DISPLAY
+
+
+//#define ST_USE_ST7789_DISPLAY
+#define ST_USE_SSD1306_DISPLAY
 
 #if defined(ST_USE_SSD1306_DISPLAY)
   //
@@ -19,12 +22,20 @@
   #define DISPLAY_SCREEN_HEIGHT         64 // OLED display height, in pixels
   #define DISPLAY_OLED_RESET            -1 // Reset pin # (or -1 for shared reset pin)
   #define DISPLAY_SCREEN_ADDRESS        0x3C // See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+
+  U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0);
 #elif defined(ST_USE_ST7789_DISPLAY)
   //
   // ST7789 Definitions
   //
   #define DISPLAY_SCREEN_WIDTH          240 // OLED display width, in pixels
   #define DISPLAY_SCREEN_HEIGHT         320 // OLED display height, in pixels
+
+  //
+  // Background and text font color definitions
+  //
+  #define ST_DISPLAY_BG_COLOR           ST77XX_BLACK
+  #define ST_DISPLAY_TEXT_COLOR         ST77XX_YELLOW
 
   //
   // SD77XX Definitions
@@ -39,23 +50,13 @@
 //
 // Touch Sensor definitions
 //
-#define TOUCH_SENSOR_INPUT_PIN        12 // GPIO based pin number of the touch sensor (interrupt based trigger)
+#define BUTTON_INPUT_PIN              12 // GPIO based pin number of the touch sensor (interrupt based trigger)
 
 //
 // Status LED definitions
 //
-#define LED_GREEN_OUTPUT_PIN          32 // GPIO based pin number of the LED that is illuminated 
-                                         // while speed tracking is active
-
-#define LED_RED_OUTPUT_PIN            33 // GPIO based pin number of the LED that is illuminated 
-                                         // while speed tracking is active
-
-#define LED_BASE_OUTPUT_PIN           34 // GPIO based pin number of the LED that is illuminated 
-                                         // while speed tracking is active
-
-#define LED_BLUE_OUTPUT_PIN           35 // GPIO based pin number of the LED that is illuminated 
-                                         // while speed tracking is active
-                                         
+#define LED_GREEN_OUTPUT_PIN          33 // GPIO based pin number of the LED that is illuminated 
+                                        
 
 //
 // NFC PIN Definitions
@@ -66,13 +67,15 @@
 //
 // PN532 Globals
 //
-Adafruit_PN532 nfc(-1, -1, &Wire);
+Adafruit_PN532 nfc(1, -1, &Wire);
 
 //
 // Speed tracking related globals
 //
 volatile bool touchDetected = false; // Flag that is set when touch sensor is activated
 volatile bool speed_tracking_active = false; // Flag to indicate that the location and speed are being tracked
+const unsigned long debounceDelay = 100; // Debounce delay in milliseconds
+volatile unsigned long lastDebounceTime = 0; // Stores the last time the button was pressed
 
 /*
 Finish line bounding box
@@ -120,7 +123,7 @@ typedef struct _SPEEDTRACKER_INFO {
 //
 // Maximum number of SPEEDTRACKER_INFO objects in the SPEEDTRACKER_INFO array.
 //
-#define SPEEDTRACKER_INFO_MAX_ENTRIES   3000
+#define SPEEDTRACKER_INFO_MAX_ENTRIES   4000
 
 //
 // The array of SPEEDTRACKER_INFO objects used to track the speed/location info for a race
@@ -135,14 +138,18 @@ uint stInfoCurrentIndex = 0; // Current index of the speedtracker array
 //
 // Initialize the display with the defined pins
 //
-Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+//Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 
+// Define global variables for configuration settings
+int config_value_race_id = 1;
+int config_value_car_id = 1;
+int config_value_driver_id = 1;
+int config_value_race_day = 1;
+int config_value_race_month = 1;
+int config_value_race_year = 2023;
+String config_value_driver_name;
+String config_value_driver_car;
 
-//
-// Background and text font color definitions
-//
-#define ST_DISPLAY_BG_COLOR           ST77XX_BLACK
-#define ST_DISPLAY_TEXT_COLOR         ST77XX_YELLOW
 
 //
 // WiFi related globals
@@ -158,10 +165,66 @@ const char* serverUrl = "http://10.0.0.1:5000/upload";
 //
 // ISRs
 //
-void touchISR() {
-  touchDetected = true;
+void IRAM_ATTR buttonInterrupt() {
+  // Check if the debounce delay has passed since the last button press
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    touchDetected = true;
+    lastDebounceTime = millis(); // Update the last debounce time
+  }
 }
 
+void drawReadyScreen(String fullName, String carInfo) {
+  u8g2.clearBuffer(); // clear the buffer
+  
+  // Top section
+  u8g2.setFont(u8g2_font_ncenB08_tr); // set font size to 8
+  u8g2.drawStr(0, 10, fullName.c_str()); // draw full name
+  u8g2.drawStr(0, 20, carInfo.c_str()); // draw car info
+  
+  // Ready section
+  u8g2.setFont(u8g2_font_fub30_tr); // set font size to 30
+  u8g2.drawStr(0, 55, "Ready"); // draw "Ready"
+  
+  u8g2.sendBuffer(); // send the buffer to the display
+}
+
+void drawMainScreen(String fullName, String carInfo, int speed) {
+  u8g2.clearBuffer(); // clear the buffer
+  
+  // Top section
+  u8g2.setFont(u8g2_font_ncenB08_tr); // set font size to 8
+  u8g2.drawStr(0, 10, fullName.c_str()); // draw full name
+  u8g2.drawStr(0, 20, carInfo.c_str()); // draw car info
+  
+  // Speed section
+  u8g2.setFont(u8g2_font_fub30_tr); // set font size to 30
+  char speedStr[4];
+  sprintf(speedStr, "%03d", speed); // add leading zeros if needed
+  u8g2.drawStr(0, 55, speedStr); // draw speed
+  u8g2.setFont(u8g2_font_fub17_tr); // set font size to 30  
+  u8g2.drawStr(73, 55, "mph"); // draw "mph" after speed
+  
+  u8g2.sendBuffer(); // send the buffer to the display
+}
+
+void drawSummaryScreen(int topSpeed) {
+  u8g2.clearBuffer(); // clear the buffer
+  
+  // Top section
+  u8g2.setFont(u8g2_font_ncenB08_tr); // set font size to 8
+  u8g2.drawStr(0, 10, "Top Speed:"); // draw "Top Speed:"
+  
+  // Speed section
+  u8g2.setFont(u8g2_font_fub30_tr); // set font size to 30
+  char speedStr[4];
+  sprintf(speedStr, "%03d", topSpeed); // add leading zeros if needed
+  u8g2.drawStr(0, 55, speedStr); // draw speed
+  u8g2.setFont(u8g2_font_fub17_tr); // set font size to 30  
+  u8g2.drawStr(73, 55, "mph"); // draw "mph" after speed
+  u8g2.sendBuffer(); // send the buffer to the display
+  
+  delay(10000); // wait for 10 seconds
+}
 // void connectToWiFi(const char* ssid, const char* password) {
 
 //   // Initialize Wi-Fi
@@ -189,66 +252,34 @@ void setup()
 {
   Serial.begin(115200);
 
-  Wire.begin(21, 22);
+  Wire.begin();
 
   // Increase the clock rate of the I2C bus so that a higher update rate can be supported
   Wire.setClock(400000);
 
   // Initialize the display
-  //tft.init(240, 320, SPI_MODE3);
-  tft.init(DISPLAY_SCREEN_WIDTH, DISPLAY_SCREEN_HEIGHT);
-
-  // Set the rotation (0, 1, 2, or 3)
-  tft.setRotation(1);
-
-  // Fill the screen with a white background
-  tft.fillScreen(ST_DISPLAY_BG_COLOR);
-
-  //tft.setFont(&FreeMonoBold9pt7b);
-  tft.setTextSize(2);
+  if(!u8g2.begin()) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
   
-  // Set the text color to black
-  tft.setTextColor(ST_DISPLAY_TEXT_COLOR);
-
-  // Set the cursor position (x, y)
-  tft.setCursor(0, 0);
-
   // Set LED outputs
-  pinMode(LED_RED_OUTPUT_PIN, OUTPUT);
+  //pinMode(LED_RED_OUTPUT_PIN, OUTPUT);
   pinMode(LED_GREEN_OUTPUT_PIN, OUTPUT);
-  pinMode(LED_BLUE_OUTPUT_PIN, OUTPUT);
-  pinMode(LED_BASE_OUTPUT_PIN, OUTPUT);
-  
-  digitalWrite(LED_BASE_OUTPUT_PIN, HIGH);
-  digitalWrite(LED_BLUE_OUTPUT_PIN, HIGH);
-  digitalWrite(LED_RED_OUTPUT_PIN, HIGH);
-  digitalWrite(LED_GREEN_OUTPUT_PIN, HIGH);
-
-  // Turn on the RED LED
   digitalWrite(LED_GREEN_OUTPUT_PIN, LOW);
-  delay(2000);
-  digitalWrite(LED_GREEN_OUTPUT_PIN, HIGH);
-  digitalWrite(LED_BLUE_OUTPUT_PIN, LOW);
-  delay(2000);
-  digitalWrite(LED_BLUE_OUTPUT_PIN, HIGH);
-  digitalWrite(LED_RED_OUTPUT_PIN, LOW);
-
-  tft.println("Initializing SD card...");
 
   // Initialize SD card if it's not already
-  if (!SD.begin(/*SD_CS, SPI, 400000, "/sd", 10, false*/)) {
+  if (!SD.begin(5, SPI, 400000, "/sd", 10, false)) {
     Serial.println("Failed to initialize SD card.");
     return;
   } else {
     Serial.println("Initialized SD card.");
-    tft.println("Initialized SD card...");
   }
 
   // Get the race config from the config file
-  getRaceConfig();
+  bool raceConfigExists = getRaceConfig();
 
   // Print the block of text
-  tft.println("Initializing PN532 NFC Reader...");
   nfc.begin();
 
   // Check for successful initialization of the NFC reader
@@ -257,51 +288,42 @@ void setup()
     //while (1); // halt
   }
 
-  tft.println("Initialized PN532 NFC Reader...");
+  nfc.SAMConfig();
 
-
-  tft.println("Initializing GPS...");
-
-  if (myGNSS.begin() == false) //Connect to the u-blox module using Wire port
+  if (myGNSS.begin(Wire, 0x42, defaultMaxWait, false) == false) //Connect to the u-blox module using Wire port
   {
     Serial.println(F("u-blox GNSS not detected at default I2C address. Please check wiring. Freezing."));
     while (1);
   } else {
-    tft.println("Initialized GPS...");    
+    Serial.println("Initialized GPS...");    
   }
 
   myGNSS.setI2COutput(COM_TYPE_UBX); //Set the I2C port to output UBX only (turn off NMEA noise)
   myGNSS.setNavigationFrequency(5); //Set output to 10 Hz
 
   uint8_t rate = myGNSS.getNavigationFrequency(); //Get the update rate of this module
-  tft.println("Current update rate: ");
-  tft.println(rate);
+  Serial.println("Current update rate: ");
+  Serial.println(rate);
+
   
   // Set the touchPin as an INPUT
-  pinMode(TOUCH_SENSOR_INPUT_PIN, INPUT_PULLDOWN);
+  pinMode(BUTTON_INPUT_PIN, INPUT_PULLUP);
 
   // Attach the interrupt to the touchPin
   // Trigger the interrupt when the pin goes from HIGH to LOW
-  attachInterrupt(digitalPinToInterrupt(TOUCH_SENSOR_INPUT_PIN), touchISR, FALLING);
-
-  tft.setCursor(0, 0);
-  tft.fillScreen(ST_DISPLAY_BG_COLOR);
-  tft.println("Please scan driver card...");
-  Serial.println("Please scan driver card...");
-
-  // Read card scanner for driver name
-  String driverName = readCardData();
-  tft.fillScreen(ST_DISPLAY_BG_COLOR);
-  tft.println("Driver: ");
-  tft.println(driverName);
-  Serial.println(driverName);
+  attachInterrupt(digitalPinToInterrupt(BUTTON_INPUT_PIN), buttonInterrupt, FALLING);
 
 
-  delay(5000);
+  if (!raceConfigExists) {
+    Serial.println("Please scan driver card...");
 
-  tft.setCursor(DISPLAY_SCREEN_HEIGHT/2, DISPLAY_SCREEN_WIDTH/2);
-  tft.fillScreen(ST_DISPLAY_BG_COLOR);
-  tft.println("Ready");
+    // Read card scanner for driver name
+    String driverName = readCardData();
+
+    // Save the driver information to file
+  }    
+  
+  drawReadyScreen(config_value_driver_name, config_value_driver_car);
 }
 
 String readCardData() {
@@ -342,16 +364,10 @@ String readCardData() {
   return "";
 }
 
-// Define global variables for configuration settings
-int config_value_race_id = 1;
-int config_value_car_id = 1;
-int config_value_driver_id = 1;
-int config_value_race_day = 1;
-int config_value_race_month = 1;
-int config_value_race_year = 2023;
 
 
-void getRaceConfig() {
+
+bool getRaceConfig() {
  
    // Open file
   char* configFileName = "/race_config.txt";  
@@ -361,7 +377,7 @@ void getRaceConfig() {
      // Config file does not exist or cannot be read
      // Use default values for configuration settings
      Serial.printf("Failed to open file %s\n", configFileName);
-     return;
+     return false;
    } else {
      Serial.printf("Opened file %s\n", configFileName);
    }
@@ -384,6 +400,17 @@ void getRaceConfig() {
      String key = line.substring(0, equalsIndex);
      String value = line.substring(equalsIndex + 1);
    
+
+    /* File Format
+      race_id=10093
+      car_id=220
+      driver_id=883
+      race_day=22
+      race_month=7
+      race_year = 2023
+      */
+
+
      // Store key-value pair in global variables
      if (key == "race_id") {
        config_value_race_id = value.toInt();
@@ -397,6 +424,10 @@ void getRaceConfig() {
        config_value_race_month = value.toInt();
      } else if (key == "race_year") {
        config_value_race_year = value.toInt();
+     } else if (key == "driver_name") {
+       config_value_driver_name = value;
+     } else if (key == "driver_car") {
+       config_value_driver_car = value;
      } else {
        // Unknown key
      }
@@ -412,57 +443,38 @@ void getRaceConfig() {
   
    // Close file
    configFile.close();
+
+   return true;
 }
 
 
-double max_speed = 0.0;
+int max_speed = 0;
 
 void loop()
 {
     // Check if the touch sensor was pressed
   if (touchDetected) {
     touchDetected = false;
-    // Add a debounce delay to avoid 
-    // multiple touch events from a single touch
 
     if (speed_tracking_active != false) {
       // Reset globals      
       speed_tracking_active = false;
-      digitalWrite(LED_BLUE_OUTPUT_PIN, HIGH);
+      digitalWrite(LED_GREEN_OUTPUT_PIN, LOW); 
 
-      tft.setCursor(0, 0);
-      tft.setTextSize(3);
-      tft.println("High Speed");
-      tft.println(max_speed);
+      drawSummaryScreen(max_speed);
+      drawMainScreen(config_value_driver_name, config_value_driver_car, max_speed);
 
-      delay(5000);
-
-      raceInformation.race_day++;
-      raceInformation.car_id++;
-      raceInformation.driver_id++;
-      raceInformation.race_id++;
-      raceInformation.race_month++;
-      raceInformation.race_year++;
-
-      saveSpeedTrackerInfoToSD();    
-
-      tft.fillScreen(ST_DISPLAY_BG_COLOR);
-      tft.println("Ready");
-  
     } else {
       speed_tracking_active = true;    
-      digitalWrite(LED_BLUE_OUTPUT_PIN, LOW);
-      digitalWrite(LED_RED_OUTPUT_PIN, HIGH);
-      digitalWrite(LED_GREEN_OUTPUT_PIN, HIGH);
-
-      tft.fillScreen(ST_DISPLAY_BG_COLOR);            
+      digitalWrite(LED_GREEN_OUTPUT_PIN, HIGH);         
     }
     
     delay(100);
   }
 
   if (speed_tracking_active) {
-    displayInfo();
+    drawMainScreen(config_value_driver_name, config_value_driver_car, max_speed);
+    delay(300);
   }
 }
 
@@ -470,8 +482,6 @@ unsigned long lastMillis;
 
 void displayInfo()
 {
-  tft.setCursor(DISPLAY_SCREEN_WIDTH/2, DISPLAY_SCREEN_HEIGHT/2);
-
   double mph = mmPerSecondToMph(myGNSS.getGroundSpeed()) ;
   double latitude = getLatitudeDegrees();
   double longitude = getLongitudeDegrees();
@@ -486,47 +496,26 @@ void displayInfo()
 
     stInfoCurrentIndex++;
 
-    // TODO: Check for overflow
+    // Check for overflow
+    if (stInfoCurrentIndex >= SPEEDTRACKER_INFO_MAX_ENTRIES) {
+      saveSpeedTrackerInfoToSD();
+      stInfoCurrentIndex = 0;      
+    }
   }
 
-  tft.setCursor(120, 140);
-  tft.fillRect(120, 140, 100, 100, ST_DISPLAY_BG_COLOR);
+  drawMainScreen(config_value_driver_name, config_value_driver_car, mph);
 
-  //if (mph > 2.0) {
-    tft.print(mph);
-    if (max_speed < mph) {
-      max_speed = mph;
-    }
-  //} else {
-  //  display.print(0.00);
-  //}    
-
-  tft.println(" Mph");
-  tft.print("(");
-  tft.print(latitude);
-  tft.print("):(");
-  tft.print(longitude);
-  tft.println(")");
 }
 
 //Function to save the stInfo array to a JSON file on the SD card
 void saveSpeedTrackerInfoToSD() {
   
-  char filename[64];
-
-  // Get the current date and time for the file name
-  snprintf(filename, sizeof(filename), "/%02d-%02d-%02d-%02d-%02d-%02d.csv",
-           raceInformation.race_id,
-           raceInformation.driver_id,
-           raceInformation.car_id,
-           raceInformation.race_month,
-           raceInformation.race_day,
-           raceInformation.race_year);
+  char* filename = "/run_data.csv";
 
   char filedata[SPEEDTRACKER_INFO_MAX_ENTRIES * (sizeof(SPEEDTRACKER_INFO) + 3)];
 
   // Open the file for writing
-  File file = SD.open(filename, FILE_APPEND);
+  File file = SD.open(filename, FILE_WRITE);
   if (!file) {
     Serial.printf("Failed to open file %s for writing.", filename);
     Serial.println();
@@ -537,7 +526,7 @@ void saveSpeedTrackerInfoToSD() {
   }
 
   // Write out the high speed
-  file.printf("High Speed: %.2f\n", max_speed);
+  file.printf("High Speed: %d\n", max_speed);
 
   // Add stInfo array data to the JSON array
   for (int i = 0; i < SPEEDTRACKER_INFO_MAX_ENTRIES; i++) {
@@ -553,54 +542,34 @@ void saveSpeedTrackerInfoToSD() {
   Serial.println("Data saved to SD card successfully.");
 }
 
-void uploadData(char* uploadData) {
+// void uploadData(char* uploadData) {
 
-  // Send the POST request
-  HTTPClient http;
-  http.begin(serverUrl);
-  http.addHeader("Content-Type", "application/text");
+//   // Send the POST request
+//   HTTPClient http;
+//   http.begin(serverUrl);
+//   http.addHeader("Content-Type", "application/text");
 
-  int httpResponseCode = http.POST(uploadData);
+//   int httpResponseCode = http.POST(uploadData);
   
-  // Check the response code
-  if (httpResponseCode > 0) {
-    String response = http.getString();
-    tft.println("Server response: " + response);
-  } else {
-    tft.println("Error on sending POST request");
-  }
+//   // Check the response code
+//   if (httpResponseCode > 0) {
+//     String response = http.getString();
+//     display.println("Server response: " + response);
+//   } else {
+//     display.println("Error on sending POST request");
+//   }
 
-  http.end();
-}
+//   http.end();
+// }
 
 
 
-bool InitializeSDCardAndFileSystem() {
-    Serial.begin(115200);
-    if(!SD.begin()){
-        Serial.println("Card Mount Failed");
-        return false;
-    }
-    uint8_t cardType = SD.cardType();
+// bool InitializeSDCardAndFileSystem() {
+//     Serial.begin(115200);
+//     if(!SD.begin(5, SPI, 400000, "/sd", 30, false)){
+//         Serial.println("Card Mount Failed");
+//         return false;
+//     }
 
-    if(cardType == CARD_NONE){
-        Serial.println("No SD card attached");
-        return false;
-    }
-
-    Serial.print("SD Card Type: ");
-    if(cardType == CARD_MMC){
-        Serial.println("MMC");
-    } else if(cardType == CARD_SD){
-        Serial.println("SDSC");
-    } else if(cardType == CARD_SDHC){
-        Serial.println("SDHC");
-    } else {
-        Serial.println("UNKNOWN");
-    }
-
-    uint64_t cardSize = SD.cardSize() / (1024 * 1024);
-    Serial.printf("SD Card Size: %lluMB\n", cardSize);
-
-    return true;
-}
+//     return true;
+// }
